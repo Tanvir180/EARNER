2de064eed1f919448a3bf791a013d4e11266a5b0
Hridoy <hmhridoy65@gmail.com>
2022-09-10 12:06:30 +0600

    add payment

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:earner_app/auth/splash_screen.dart';
 import 'package:earner_app/pages/login_page.dart';
 import 'package:earner_app/pages/select_user.dart';
+import 'package:earner_app/widgets/payment.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey =
+      "pk_test_51Lg8ZaEwHQ1skIbyFSluAQEJ9UJBOBVmQF7PIIATCDo2WuWXNOMteKZqifC4typBMOEc6kKFSnDTcYodTjvNpuel00hHRyOFim";
+  
+  await Stripe.instance.applySettings();
   await Firebase.initializeApp();
 
   runApp(const MyApp());
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'dart:convert';
 
-class MyDrawer extends StatelessWidget {
+import 'package:http/http.dart' as http;
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+class MyDrawer extends StatefulWidget {
   const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  Map<String, dynamic>? paymentIntent;
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +22,8 @@ class MyDrawer extends StatelessWidget {
         color: Colors.cyan,
         child: ListView(
           padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
+          children: <Widget>[
+            const DrawerHeader(
               padding: EdgeInsets.all(0),
               margin: EdgeInsets.all(0),
               child: UserAccountsDrawerHeader(
@@ -27,12 +38,12 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            ListTile(
+            const ListTile(
               leading: Icon(
                 CupertinoIcons.home,
                 color: Colors.white,
               ),
-              title: Text(
+              title: const Text(
                 "Home",
                 textScaleFactor: 1.2,
                 style: TextStyle(
@@ -40,8 +51,8 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            Divider(indent: 16, endIndent: 16),
-            ListTile(
+            const Divider(indent: 16, endIndent: 16),
+            const ListTile(
               leading: Icon(
                 CupertinoIcons.profile_circled,
                 color: Colors.white,
@@ -54,8 +65,8 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            Divider(indent: 16, endIndent: 16),
-            ListTile(
+            const Divider(indent: 16, endIndent: 16),
+            const ListTile(
               leading: Icon(
                 CupertinoIcons.mail_solid,
                 color: Colors.white,
@@ -68,14 +79,125 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            Divider(indent: 16, endIndent: 16),
+            const Divider(indent: 16, endIndent: 16),
+            ListTile(
+              leading: const Icon(
+                CupertinoIcons.play_arrow_solid,
+                color: Colors.white,
+              ),
+              title: MaterialButton(
+                padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                minWidth: MediaQuery.of(context).size.width,
+                onPressed: () async {
+                  await makePayment();
+                },
+                child: const Text(
+                  "Payment",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-}
 
+  Future<void> makePayment() async {
+    try {
+      paymentIntent = await createPaymentIntent('10', 'USD');
+      //Payment Sheet
+      await Stripe.instance
+          .initPaymentSheet(
+              paymentSheetParameters: SetupPaymentSheetParameters(
+                  paymentIntentClientSecret: paymentIntent!['client_secret'],
+                  // applePay: const PaymentSheetApplePay(merchantCountryCode: '+92',),
+                  // googlePay: const PaymentSheetGooglePay(testEnv: true, currencyCode: "US", merchantCountryCode: "+92"),
+                  style: ThemeMode.dark,
+                  merchantDisplayName: 'Adnan'))
+          .then((value) {});
+
+      ///now finally display payment sheeet
+      displayPaymentSheet();
+    } catch (e, s) {
+      print('exception:$e$s');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet().then((value) {
+        showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.check_circle,
+                            color: Colors.green,
+                          ),
+                          Text("Payment Successfull"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ));
+        // ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("paid successfully")));
+
+        paymentIntent = null;
+      }).onError((error, stackTrace) {
+        print('Error is:--->$error $stackTrace');
+      });
+    } on StripeException catch (e) {
+      print('Error is:---> $e');
+      showDialog(
+          context: context,
+          builder: (_) => const AlertDialog(
+                content: Text("Cancelled "),
+              ));
+    } catch (e) {
+      print('$e');
+    }
+  }
+
+  //  Future<Map<String, dynamic>>
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': calculateAmount(amount),
+        'currency': currency,
+        'payment_method_types[]': 'card'
+      };
+
+      var response = await http.post(
+        Uri.parse('https://api.stripe.com/v1/payment_intents'),
+        headers: {
+          'Authorization': 'Bearer hridoy',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body,
+      );
+      // ignore: avoid_print
+      print('Payment Intent Body->>> ${response.body.toString()}');
+      return jsonDecode(response.body);
+    } catch (err) {
+      // ignore: avoid_print
+      print('err charging user: ${err.toString()}');
+    }
+  }
+
+  calculateAmount(String amount) {
+    final calculatedAmout = (int.parse(amount)) * 100;
+    return calculatedAmout.toString();
+  }
+}
 
 
 // ListView.separated(
